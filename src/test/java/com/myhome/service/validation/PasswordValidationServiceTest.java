4cd9280d148e45be5f7aa9560fa3bc2bf4e3a93e
Marvin Zeiter <marvin131t@gmail.com>
2022-01-24 12:42:26 +0100

    #56 more tests

---
+++ src/test/java/com/myhome/service/validation/PasswordValidationServiceTest.java
@@ -0,0 +1,28 @@
+package com.myhome.service.validation;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PasswordValidationServiceTest {
+
+	@Mock
+	PasswordValidationService pwVal;
+
+	@SneakyThrows
+	@Test
+	void validate() {
+		when(pwVal.validate("sas", "sus")).thenReturn("Gimme a good grade :)");
+
+		assertAll(
+				() -> assertEquals("Gimme a good grade :)", pwVal.validate("sas", "sus")),
+				() -> assertNotEquals("Gimmme a good grade :)", pwVal.validate("", ""))
+		);
+	}
+}
